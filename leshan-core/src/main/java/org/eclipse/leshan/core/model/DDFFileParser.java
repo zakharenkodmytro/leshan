69757fc86fc5b37f3d14345596ae481ca9c4f1fc
Simon Bernard <sbernard@sierrawireless.com>
2020-07-31 18:10:10 +0200

    Update ObjectModel and ObjectModelValidator to support LWM2M 1.1

--- leshan-core/src/main/java/org/eclipse/leshan/core/model/DDFFileParser.java
+++ leshan-core/src/main/java/org/eclipse/leshan/core/model/DDFFileParser.java
@@ -298,8 +298,11 @@ public class DDFFileParser {
                     type = Type.OBJLNK;
                     break;
                 case "Unsigned Integer":
+                    type = Type.UNSIGNED_INTEGER;
+                    break;
                 case "Corelnk":
-                    throw new UnsupportedOperationException(field.getTextContent() + " type is not yet supported");
+                    type = Type.CORELINK;
+                    break;
                 case "":
                     type = Type.NONE;
                     break;
